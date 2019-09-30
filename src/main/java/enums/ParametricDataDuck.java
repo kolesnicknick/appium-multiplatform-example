7deed85550a69f3649f5a4136f33ce28b0008203
niko <kolesniknikolai92@gmail.com>
2019-09-30 11:37:07 +0300

    todo: Update WebDriverController and AppiumTestBase class

--- src/main/java/enums/ParametricDataDuck.java
+++ src/main/java/enums/ParametricDataDuck.java
@@ -1,25 +1,30 @@
 package enums;
 
-public enum  ParametricDataDuck {
+public enum ParametricDataDuck {
 
-    RU  ("ru", "Компьютерная школа Hillel в Киеве: курсы IT технологий"),  //calls constructor with value 3
-    FR("fr", "Создать сайт бесплатно | Конструктор сайтов | Wix.com"),  //calls constructor with value 2
-    ES   ("es", "Top of the day / Habr")   //calls constructor with value 1
-    ; // semicolon needed when fields / methods follow
+    RU("ru", "Создать сайт", "Здесь создают профессиональные сайты"),
+    FR("fr", "C'est parti !", "Créer un site internet de qualité professionnelle"),
+    ES("es", "Empieza ya", "El Lugar Perfecto para Crear tu Página Web Profesional");
 
-
-    private final String expectedTitle;
+    private final String createButtonTitle;
+    private final String signInButtonTitle;
     private final String searchParam;
 
-    ParametricDataDuck(String searchParam, String expectedTitle) {
+    ParametricDataDuck(String searchParam, String createButtonTitle, String signInButtonTitle) {
         this.searchParam = searchParam;
-        this.expectedTitle = expectedTitle;
+        this.createButtonTitle = createButtonTitle;
+        this.signInButtonTitle = signInButtonTitle;
     }
 
-    public String getExpectedTitle() {
-        return this.expectedTitle;
+    public String getWelcomeText() {
+        return this.createButtonTitle;
     }
-    public String getCode() {
+
+    public String getSignInButtonTitle() {
+        return this.signInButtonTitle;
+    }
+
+    public String getSearchParam() {
         return this.searchParam;
     }
 
--- src/test/java/Lesson3HWTest.java
+++ src/test/java/Lesson3HWTest.java
@@ -1,26 +1,30 @@
 import enums.ParametricDataDuck;
 import io.appium.java_client.AppiumDriver;
 import managers.WebDriverController;
-import org.hamcrest.Matchers;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 
 @RunWith(Parameterized.class)
 public class Lesson3HWTest extends AppiumTestBase {
 
 
-
-
     @ParameterizedTest
     @EnumSource(ParametricDataDuck.class)
-    public void parametricTest(ParametricDataDuck data){
-        AppiumDriver driver = (AppiumDriver) WebDriverController.getInstance().getDriver();
-        driver.get("http://www"+data.getCode() + ".wix.com");
-        Assert.assertEquals(driver.getTitle(), Matchers.containsString(data.getExpectedTitle()));
+    public void parametricTest(ParametricDataDuck data) {
+        AppiumDriver driver = WebDriverController.getInstance().getDriver();
+        driver.get("http://www" + data.getSearchParam() + ".wix.com");
+
+        WebElement signInButton = driver.findElement(By.cssSelector("[role=button]"));
+        WebElement welcomeTextHolder = driver.findElement(By.cssSelector(".txtNew"));
+
+        Assert.assertEquals(data.getSignInButtonTitle(), signInButton.getText());
+        Assert.assertEquals(data.getCreateButtonTitle(), welcomeTextHolder.getText());
+
     }
 }
